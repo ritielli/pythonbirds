f853625535822d99e42db58ca698aebd96acfb8b
Ritielli Castro Pascoal <ritykstro@hotmail.com>
2022-11-03 16:04:06 -0400

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=31):
+    def __init__(self, *filhos, nome=None, idade=31):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Pedro')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Ritielli'
-    print(p.nome)
-    print(p.idade)
+    pedro = Pessoa(nome='Pedro')
+    ritielli = Pessoa(pedro, nome='Ritielli')
+    print(Pessoa.cumprimentar(ritielli))
+    print(id(ritielli))
+    print(ritielli.cumprimentar())
+    print(ritielli.nome)
+    print(ritielli.idade)
+    for filho in ritielli.filhos:
+        print(filho.nome)
