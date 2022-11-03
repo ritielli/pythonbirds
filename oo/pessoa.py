83912284335a6e692ad17316952c5b5116aa39c3
Ritielli Castro Pascoal <ritykstro@hotmail.com>
2022-11-03 15:51:15 -0400

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=31):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
+
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Pedro')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Ritielli'
+    print(p.nome)
+    print(p.idade)
